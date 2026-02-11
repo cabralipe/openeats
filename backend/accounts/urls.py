e4d2cce444a2ee0d68d9fcbcecf4d66b3dd2afd3
Felipe Cabral <cabralipe@outlook.com>
2026-02-11 11:41:15 -0300

    render test

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,5 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair_no_slash'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh_no_slash'),
 ]
